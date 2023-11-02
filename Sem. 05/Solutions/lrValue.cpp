3e4a5b586b68b1afe63ee827ab10f9949b49f553
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-11-02 16:40:36 +0200

    Update lrValue.cpp

--- Sem. 05/Solutions/lrValue.cpp
+++ Sem. 05/Solutions/lrValue.cpp
@@ -10,7 +10,13 @@ int main() {
     int& b = returnsLValue(a);
     int c = returnsLValue(a);
     b++;
+
     std::cout << a << std::endl; // 8
     std::cout << b << std::endl; // 8
     std::cout << c << std::endl; // 7
+
+    int d = 1;
+    (++d) = 4;
+    // (d++) = 5 // error - cannot assign to rvalue
+    std::cout << d << std::endl; // 4
 }
