d26581fbae06460504361b4ef027995e30b1156a
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-11-10 00:14:50 +0200

    add validation for length input

--- Sem. 06/Solutions/ExerciseTemplate.cpp
+++ Sem. 06/Solutions/ExerciseTemplate.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+const unsigned MAX_SIZE = 1024;
+
+bool isValidLength(unsigned length) {
+	return 0 < length && length <= MAX_SIZE;
+}
+
 void readArray(int* arr, unsigned length) {
 	for (unsigned i = 0; i < length; i++) {
 		std::cin >> arr[i];
@@ -13,16 +19,14 @@ void printArray(int* arr, unsigned length) {
 	std::cout << std::endl;
 }
 
-const unsigned MAX_SIZE = 1024;
-
 int main() {
 	int arr[MAX_SIZE];
 
 	// 1 <= length <= MAX_SIZE 
-	int length;
-	std::cin >> length;
-
-	// check if length is valid
+	unsigned length;
+	do {
+		std::cin >> length;
+	} while(!isValidLength(length));
 
 	readArray(arr, length);
 	
