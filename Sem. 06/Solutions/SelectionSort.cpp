3985bf9d6afa42541571d6c16ebdbe2f87fe4546
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-11-16 15:42:55 +0200

    Update SelectionSort.cpp

--- Sem. 06/Solutions/SelectionSort.cpp
+++ Sem. 06/Solutions/SelectionSort.cpp
@@ -1,1 +1,14 @@
+void selectionSort(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		int minElementIndex = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < arr[minElementIndex]) {
+				minElementIndex = j;
+			}
+		}
 
+		if (i != minElementIndex) {
+			swap(arr, i, minElementIndex);
+		}
+	}
+}
