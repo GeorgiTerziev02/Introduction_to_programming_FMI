0e22e5d06532333d415118434b990cdb8a236e35
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-11-30 16:05:15 +0200

    Create FirstNotDuplicated.cpp

---
+++ Sem. 08/Solutions/FirstNotDuplicated.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int findNotDuplicated(const int* arr, unsigned size) {
+    int result = 0;
+    // XOR of a number with itself is 0
+    // XOR of a number with 0 is the number itself
+    // Every number has different binary representation
+    // So the XOR of all numbers in the array will be the number that is not duplicated
+    for (unsigned i = 0; i < size; i++) {
+        result ^= arr[i];
+    }
+
+    return result;
+}
+
+int main() {
+    const unsigned LENGTH = 7;
+    // Every number in the array is duplicated except one
+    int arr1[] = { 5, 1, 5, 6, 1, 7, 6 };
+    int arr2[] = { 5, 5, 5, 5, 1, 5, 5 };
+
+    std::cout << findNotDuplicated(arr1, 7);
+    // Will this work?
+    // std::cout << findNotDuplicated(arr2, 7);
+}
