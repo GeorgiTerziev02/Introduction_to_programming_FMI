defbf9640ea752b33ec57635b575915d6e17d653
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-12-10 22:53:18 +0200

    Handle draw case

--- Sem. 09/Solutions/Simple-Tic-Tac-Toe.cpp
+++ Sem. 09/Solutions/Simple-Tic-Tac-Toe.cpp
@@ -6,6 +6,7 @@
 // - not optimized check for winner
 #include <iostream>
 
+// Using rows and cols just to try out working with matrix
 const int ROWS = 3;
 const int COLS = 3;
 const int INITIAL_VALUE = 0;
@@ -15,7 +16,7 @@ int getPlayer(bool isFirstPlayer) {
 	return isFirstPlayer ? 1 : 2;
 }
 
-void initBoardWithValue(int board[ROWS][COLS], int rows, int cols, int value) {
+void initBoardWithValue(int board[][COLS], int rows, int cols, int value) {
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			board[i][j] = value;
@@ -27,17 +28,17 @@ bool isInsideBoard(int x, int y) {
 	return 0 <= x && x < ROWS && 0 <= y && y < COLS;
 }
 
-bool isPopulatedField(int x, int y, int board[ROWS][COLS]) {
+bool isPopulatedField(int x, int y, int board[][COLS]) {
 	return board[x][y] != INITIAL_VALUE;
 }
 
-void enterValidCoordinates(int& x, int& y, int board[ROWS][COLS]) {
+void enterValidCoordinates(int& x, int& y, int board[][COLS]) {
 	do {
 		std::cin >> x >> y;
 	} while (!isInsideBoard(x, y) || isPopulatedField(x, y, board));
 }
 
-void printMatrix(int board[ROWS][COLS], int rows, int cols) {
+void printBoard(int board[][COLS], int rows, int cols) {
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
 			std::cout << board[i][j];
@@ -47,7 +48,7 @@ void printMatrix(int board[ROWS][COLS], int rows, int cols) {
 	}
 }
 
-bool hasWinnerOnRows(int board[ROWS][COLS], int rows, int cols) {
+bool hasWinnerOnRows(int board[][COLS], int rows, int cols) {
 	for (int i = 0; i < rows; i++) {
 		bool isFullRow = true;
 		for (int j = 0; j < cols - 1; j++) {
@@ -65,7 +66,7 @@ bool hasWinnerOnRows(int board[ROWS][COLS], int rows, int cols) {
 	return false;
 }
 
-bool hasWinnerOnCols(int board[ROWS][COLS], int rows, int cols) {
+bool hasWinnerOnCols(int board[][COLS], int rows, int cols) {
 	for (int j = 0; j < COLS; j++) {
 		bool isFullCol = true;
 		for (int i = 0; i < rows - 1; i++) {
@@ -83,7 +84,7 @@ bool hasWinnerOnCols(int board[ROWS][COLS], int rows, int cols) {
 	return false;
 }
 
-bool hasWinnerOnMainDiagonal(int board[ROWS][COLS], int rows, int cols) {
+bool hasWinnerOnMainDiagonal(int board[][COLS], int rows, int cols) {
 	for (int i = 0; i < rows - 1; i++) {
 		if (!isPopulatedField(i, i, board) || board[i][i] != board[i + 1][i + 1]) {
 			return false;
@@ -93,23 +94,23 @@ bool hasWinnerOnMainDiagonal(int board[ROWS][COLS], int rows, int cols) {
 	return true;
 }
 
-bool hasWinnerOnSecondaryDiagonal(int board[ROWS][COLS], int rows, int cols) {
+bool hasWinnerOnSecondaryDiagonal(int board[][COLS], int rows, int cols) {
 	for (int i = 0, j = cols - 1; i < rows - 1; i++, j--) {
 		if (!isPopulatedField(i, i, board) || board[i][j] != board[i + 1][j - 1]) {
 			return false;
 		}
 	}
-	
+
 	return true;
 }
 
-bool hasWinner(int board[ROWS][COLS], int rows, int cols) {
-	return hasWinnerOnRows(board, rows, cols) 
+bool hasWinner(int board[][COLS], int rows, int cols) {
+	return hasWinnerOnRows(board, rows, cols)
 		|| hasWinnerOnCols(board, rows, cols)
 		|| hasWinnerOnMainDiagonal(board, rows, cols)
 		|| hasWinnerOnSecondaryDiagonal(board, rows, cols);
 }
- 
+
 int main() {
 	int board[ROWS][COLS];
 	initBoardWithValue(board, ROWS, COLS, INITIAL_VALUE);
@@ -117,7 +118,8 @@ int main() {
 	// isFirstPlayer => 1
 	// !isFirstPlayer => 2
 	bool isFirstPlayer = true;
-	while (true) {
+	int roundCounter = 0;
+	while (roundCounter < ROWS * COLS) {
 		std::cout << "Player " << getPlayer(isFirstPlayer) << " enter coordinates: ";
 		int x, y;
 		enterValidCoordinates(x, y, board);
@@ -127,11 +129,17 @@ int main() {
 		if (hasWinner(board, ROWS, COLS)) {
 			std::cout << std::endl << std::endl;
 			std::cout << "Player " << getPlayer(isFirstPlayer) << " wins!" << std::endl;
-			printMatrix(board, ROWS, COLS);
+			printBoard(board, ROWS, COLS);
 			break;
 		}
 
 		isFirstPlayer = !isFirstPlayer;
-		printMatrix(board, ROWS, COLS);
+		printBoard(board, ROWS, COLS);
+		roundCounter++;
+	}
+
+	if (roundCounter == ROWS * COLS) {
+		std::cout << std::endl << std::endl;
+		std::cout << "Draw!" << std::endl;
 	}
 }
