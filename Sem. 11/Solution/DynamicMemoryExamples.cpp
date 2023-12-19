9315be484d383161724ded72208642598da3c987
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-12-19 17:33:33 +0200

    Add dynamic allocation examples

---
+++ Sem. 11/Solution/DynamicMemoryExamples.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int dynamicMemoryTest() {
+	int length;
+	std::cin >> length;
+
+	// ptr to just a int in the heap
+	int* intPtr = new int(5);
+	// ptr to arr of ints in the heap
+	int* arr = new int[length];
+	
+	// !!!
+	delete intPtr;
+	delete[] arr;
+}
+
+int* functionReturningDynamicArray() {
+	int length;
+	std::cin >> length;
+
+	int* arr = new int[length];
+	arr[0] = 1;
+	arr[1] = 5;
+	return arr;
+}
+
+int main() {
+	dynamicMemoryTest();
+	int* arr = functionReturningDynamicArray();
+	std::cout << arr[0] << " " << arr[1];
+	delete[] arr;
+}
